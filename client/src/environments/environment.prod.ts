9fecac67d247fcf05c469c9438341580b650ce26
Afreen Roohi <rafreen@kaartech.com>
2025-12-03 18:56:40 +0530

    update login

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   postLogoutUrl: '',
-  apiUrl: 'https://dxbktlds4cp.kcloud.com:8000/',
+  apiUrl: 'https://swa-project.onrender.com/api/',
   adfsendpoint: '',
   // adfsendpoint: 'https://sspr.mwan.gov.sa/sso/oauth/d92acd855f91fb546784e465b76e0f9e7b5aa4ec',
   clientId: '',
